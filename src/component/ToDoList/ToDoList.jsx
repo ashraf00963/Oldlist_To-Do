49f7e6e44157dfbc9d4a36e3e27f03eb2ebdd309
ashraf00963 <ashrf00963@gmail.com>
2024-06-23 10:52:42 +0200

    fixes

--- src/component/ToDoList/ToDoList.jsx
+++ src/component/ToDoList/ToDoList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from 'react-router';
 import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
-import { TasksList, InProgressList, CompletedList, GarbageBin, CustomDragLayer} from './Circle';
+import { TasksList, InProgressList, CompletedList, GarbageBin, CustomDragLayer } from './Circle';
 import './ToDoList.css';
 
 function ToDoList() {
@@ -19,10 +19,10 @@ function ToDoList() {
     useEffect(() => {
         const getList = async () => {
             try {
-                const response = await axios.get(`${URL}/getLists.php`, {
+                const response = await axios.get(`${URL}/getList.php`, {
                     params: {
-                        userId: userId,
-                        listId: listId
+                        user_id: userId,
+                        list_id: listId
                     }
                 });
                 const { name, tasks } = response.data;
@@ -50,7 +50,7 @@ function ToDoList() {
 
         try {
             const task = { id: uuidv4(), ...newTask, status: 'tasks' };
-            const response = await axios.post(`${URL}/users/${userId}/lists/${listId}/tasks`, task);
+            const response = await axios.post(`${URL}/addTask.php`, task);
             setTasks((prevTasks) => ({
                 ...prevTasks,
                 tasks: [...prevTasks.tasks, response.data]
@@ -64,7 +64,7 @@ function ToDoList() {
     const handleDeleteTask = async (taskId, status) => {
         console.log('Deleting task ID:', taskId);
         try {
-            await axios.delete(`${URL}/users/${userId}/lists/${listId}/tasks/${taskId}`);
+            await axios.delete(`${URL}/deleteTask.php`, { data: { task_id: taskId } });
             setTasks((prevTasks) => ({
                 ...prevTasks,
                 [status]: prevTasks[status].filter((task) => task.id !== taskId)
@@ -81,8 +81,7 @@ function ToDoList() {
         const updatedTask = { ...taskToMove, status: targetStatus };
 
         try {
-            await axios.put(`${URL}/users/${userId}/lists/${listId}/tasks/${taskId}`, updatedTask);
-
+            await axios.put(`${URL}/updateTask.php`, updatedTask);
             setTasks((prevTasks) => {
                 const newState = { ...prevTasks };
                 newState[sourceStatus] = newState[sourceStatus].filter(task => task.id !== taskId);
@@ -97,7 +96,7 @@ function ToDoList() {
 
     const updateTask = async (taskId, updatedTask) => {
         try {
-            await axios.put(`${URL}/users/${userId}/lists/${listId}/tasks/${taskId}`, updatedTask);
+            await axios.put(`${URL}/updateTask.php`, updatedTask);
             setTasks((prevTasks) => {
                 const newState = { ...prevTasks };
                 newState[updatedTask.status] = newState[updatedTask.status].map(task =>
