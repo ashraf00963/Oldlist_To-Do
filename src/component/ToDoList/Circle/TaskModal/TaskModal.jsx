39bcccb54ba58926ed65145c1bf2e3ff36834d09
ashraf00963 <ashrf00963@gmail.com>
2024-06-23 18:35:44 +0200

    attachment map error fixed

--- src/component/ToDoList/Circle/TaskModal/TaskModal.jsx
+++ src/component/ToDoList/Circle/TaskModal/TaskModal.jsx
@@ -7,18 +7,17 @@ function TaskModal({ task, isOpen, onClose }) {
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-                <button className="close-button" onClick={onClose}>×</button>
                 <h2>{task.name}</h2>
                 <textarea value={task.note} readOnly placeholder="No notes available"></textarea>
-                {task.attachments && task.attachments.length > 0 ? (
-                    <ul>
-                        {task.attachments.map((attachment, index) => (
-                            <li key={index}><a href={`https://list-todo.com/uploads/${attachment}`} target="_blank" rel="noopener noreferrer">{attachment}</a></li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No attachments</p>
-                )}
+                <div className='attachments'>
+                    {task.attachments && task.attachments.length > 0 ? (
+                        <div key={index}>
+                            <a href={attachment.url} target="_blank" rel="noopener noreferrer">{attachment.name}</a>
+                        </div>
+                    ) : (
+                        <p className='attach-p'>No attachments</p>
+                    )}
+                </div>
             </div>
         </div>
     );
